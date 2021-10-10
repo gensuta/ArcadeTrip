1a9c34697627e3213b4ea9833ad5008218692fd0
Gen <geneva@glowup.games>
2021-10-10 15:17:20 -0400

    Intro dialogue is in game
    I bought over the yarn stuff I was working on and made sure all the intro dialogue miles currently wrote is in the game! Going to move on to the hairdo game now

---
+++ ArcadeTrip_Project/Assets/scripts/Actor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Actor",menuName = "Yarn Spinner/Actor",order = 0)]
+public class Actor : ScriptableObject
+{
+	[SerializeField] string actorName;
+	[SerializeField] VNSprite[] actorSprites;
+	
+	public Sprite GetSprite(string mySprite)
+	{
+		foreach(VNSprite v in actorSprites)
+		{
+			if(mySprite.Equals(v.SpriteName().Trim()))
+			{
+				return v.SpriteImage();
+			}
+		}
+
+		return null;
+	}
+
+}
+
+[System.Serializable]
+public class VNSprite
+{
+	[SerializeField] string spriteName;
+	[SerializeField] Sprite spriteImage;
+
+	public string SpriteName()
+	{
+		return spriteName;
+	}
+
+	public Sprite SpriteImage()
+	{
+		return spriteImage;
+	}
+}
---
+++ ArcadeTrip_Project/Assets/scripts/ExtraYarnCommands.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.SceneManagement;
+public class ExtraYarnCommands : MonoBehaviour
+{
+    [SerializeField] DialogueRunner runner;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        runner.AddCommandHandler<string>("UnityScene", DoUnitySceneChange);
+    }
+
+    void DoUnitySceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
