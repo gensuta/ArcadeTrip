4abdbfdbda8ae60add4e521665bf7900ed625a23
Gen <geneva@glowup.games>
2021-10-13 15:13:25 -0400

    hair prototype "done" but needs to be redone

---
+++ ArcadeTrip_Project/Assets/scripts/HairGame/LineBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineBehavior : MonoBehaviour // MIGHT NOT NEED THIS NOW
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        
+    }
+}
